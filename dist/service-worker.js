dfe77d9c0738301c4e4c8c846162972a8d6312e7
Mohamed Ghoubali <mghoubali@gmail.com>
2019-05-20 17:16:04 +0200

    hidden file

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.8b19c1c89c54a2ddb82866531a44d8d2.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.9a1c674658e371de693c8eb623ae465c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({ prefix: 'bento-starter' })
 
